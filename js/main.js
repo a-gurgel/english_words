2c1856c08483a419a0d644403cd773a6a02125a3
Ananda Gurgel <gurgel.ananda@gmail.com>
2020-07-07 00:28:43 -0300

    consumindo o back-end

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-AOS.init();
+AOS.init()
+
+fetch(`http://hundred-words.herokuapp.com/cem-palavras`)
+.then(response => response.json())
+   
+.then(jsonBody => {
+    for(let i=0; i<jsonBody.length; i++){
+        document.querySelector("#name"+i).innerHTML = jsonBody[i].name;
+        document.querySelector("#description"+i).innerHTML = jsonBody[i].description;
+    }
+
+    console.log(jsonBody);
+});
+
